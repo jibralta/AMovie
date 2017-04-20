02ad75a1f5f512ac0a3784b83e63803193ead48c
Joy Umali <joyumali@gmail.com>
2017-04-19 23:26:25 -0700

    changed animation for spider

--- AnimationsLab/ViewController.swift
+++ AnimationsLab/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
         theEnd.alpha = 0.0
         wolf.alpha = 0.0
         introLabel.center.y += view.bounds.height
+        hotAirBalloon.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
         
         
     }
@@ -62,12 +63,12 @@ class ViewController: UIViewController {
         //
         //        }, completion: nil)
         
-        
-        UIView.animate(withDuration: 20.0) {
+        // BALLOON
+        UIView.animate(withDuration: 20.0, delay: 0, options: .repeat, animations: {
             self.hotAirBalloon.center.x -= self.view.bounds.width
             self.hotAirBalloon.transform = CGAffineTransform(scaleX: 4.0, y: 4.0)
             
-        }
+        }, completion: nil)
 
         if self.hotAirBalloon.center.x == self.pterodactyl.center.x && self.hotAirBalloon.center.y == self.pterodactyl.center.y {
             launchWolf()
@@ -96,20 +97,22 @@ class ViewController: UIViewController {
     }
     
     func launchSpider() {
-        UIView.animate(withDuration: 45.0, delay: 10.0, options: .curveEaseIn, animations: {
-            self.spider.transform = CGAffineTransform(scaleX: 500.0, y: 500.0)
+        UIView.animate(withDuration: 500, delay: 5.0, options: .curveLinear, animations: {
+            self.spider.transform = CGAffineTransform(scaleX: 500.0, y: 500.00)
         }, completion: nil)
         
         
-        UIView.animate(withDuration: 100000.0, delay: 10.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.3, options: .autoreverse, animations: {
-            self.spider.center.x -= self.view.bounds.width/2
+        UIView.animate(withDuration: 8.0, delay: 5, options: .curveLinear, animations: {
+            
+        self.spider.center.x -= self.view.bounds.width/2
             self.spider.center.y += self.view.bounds.height
         }, completion: nil)
         
     }
     
     func launchPterodactyl() {
-        UIView.animate(withDuration: 15.0, animations: {
+        UIView.animate(withDuration: 15.0, delay: 0.0, options: .transitionCurlUp, animations: {
+            
             self.pterodactyl.center.x += self.view.bounds.width
             self.pterodactyl.center.y -= self.view.bounds.height
             
